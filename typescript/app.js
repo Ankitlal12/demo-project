dd758cef1a64bfc5c7a614762861731a9e0767c9
Ankit Lal Shrestha <shresthaankitlal@gmail.com>
2025-07-23 09:55:22 +0545

    changed

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,6 @@
 "use strict";
 const button = document.querySelector('button');
+// a commenet
 button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
     console.log('Clicked!');
 });
